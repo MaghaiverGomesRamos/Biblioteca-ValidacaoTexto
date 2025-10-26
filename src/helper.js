2701b80b5cd793df131235d6b0b50b2e0a971275
Breakosts <magaiverg2@gmail.com>
2025-10-26 19:58:38 -0300

    ultimas alteracoes

--- src/helper.js
+++ src/helper.js
@@ -1,11 +1,14 @@
 function filtraOcorrencias(paragrafo){
-    return Object.keys(paragrafo).filter(chave => paragrafo[chave] > 1)
+    return Object.keys(paragrafo).filter(chave => paragrafo[chave] > 1)  
 }
 
 function montaSaidaArquivo(listaPalavras){
     let textoFinal = '';
     listaPalavras.forEach((paragrafo, indice) => {
         const duplicadas = filtraOcorrencias(paragrafo).join(', ');
+        if (duplicadas == ''){
+            return
+        }
         textoFinal += `Palavras duplicadas no parágrafo: ${indice + 1}: ${duplicadas} \n`
     });
 
