db7ebad6128fd6d0405a5f6ca7f7869a1a04066b
Breakosts <magaiverg2@gmail.com>
2025-10-26 11:00:54 -0300

    Funcao externa

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 import fs from 'fs'
+import { trataErros } from './erros/funcoesErro.js';
 
 const caminhoArquivo = process.argv;
 const link = caminhoArquivo[2];
 
 fs.readFile(link, 'utf-8', (erro, texto) => {
-    if (erro){
-        console.log('Erro encontrado:', erro.code);
-        return
-    }
-    
-    contaPalavras(texto)
+    try {
+        if (erro) throw erro
+        contaPalavras(texto)
+    } catch(erro){
+        trataErros(erro);
+    } 
 })
 
 function contaPalavras(texto){
